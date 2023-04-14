6d4f49891019d10f1826516fd003869015677f6c
Harry Kim <harry@Harryui-noteubug.local>
2023-04-14 15:45:43 +0900

    .env added & input prompt modified

--- static/routing.js
+++ static/routing.js
@@ -2,38 +2,38 @@ function initMap() {
     const directionsService = new google.maps.DirectionsService();
     const directionsRenderer = new google.maps.DirectionsRenderer();
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 15,
-      center: { lat: 35.016885, lng: 126.803689 }
+        zoom: 15,
+        center: { lat: 35.016885, lng: 126.803689 }
     });
   
     directionsRenderer.setMap(map);
   
     const onChangeHandler = function () {
-      calculateAndDisplayRoute(directionsService, directionsRenderer);
+        calculateAndDisplayRoute(directionsService, directionsRenderer);
     };
   
     document.getElementById("start").addEventListener("change", onChangeHandler);
     document.getElementById("end").addEventListener("change", onChangeHandler);
-  }
+}
   
-  function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
     // const {Place} = await google.map.importLibrary("places");
 
     directionsService
-      .route({
-        origin: {
-            query: document.getElementById("start").value,
-        },
-        destination: {
-            query: document.getElementById("end").value,
-        },
-        // DRIVING, WALKING, BICYCLING은 안되고 TRANSIT만 가능!!!!!
-        travelMode: google.maps.TravelMode.TRANSIT,
-      })
-      .then((response) => {
-        directionsRenderer.setDirections(response);
-      })
-      .catch((e) => window.alert("Directions request failed due"));
-  }
+        .route({
+            origin: {
+                query: document.getElementById("start").value,
+            },
+            destination: {
+                query: document.getElementById("end").value,
+            },
+            // DRIVING, WALKING, BICYCLING은 안되고 TRANSIT만 가능!!!!!
+            travelMode: google.maps.TravelMode.TRANSIT,
+        })
+        .then((response) => {
+            directionsRenderer.setDirections(response);
+        })
+        .catch((e) => window.alert("Directions request failed due"));
+}
   
-  window.initMap = initMap;
+window.initMap = initMap;
